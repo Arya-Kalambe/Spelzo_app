69cb3e0f64fe13183b3bc056684e07dbbd378a97
Arya-Kalambe <aryakalambe23@gmail.com>
2025-06-17 11:56:15 +0530

    Add files via upload

---
+++ signup.dart
@@ -0,0 +1,173 @@
+      // Figma Flutter Generator SignupWidget - FRAME
+      Container(
+      width = 412,
+      height = 917,
+      decoration = BoxDecoration(
+          color : Color.fromRGBO(255, 255, 255, 1),
+  ),
+      child = Stack(
+        children: <Widget>[
+          Positioned(
+        top: -2,
+        left: 0,
+        child: Container(
+        width: 412,
+        height: 917,
+        decoration: BoxDecoration(
+          image : DecorationImage(
+          image: AssetImage('assets/images/Untitleddesign3.png'),
+          fit: BoxFit.fitWidth
+      ),
+  )
+      )
+      ),Positioned(
+        top: 112,
+        left: 35,
+        child: Container(
+      width: 273,
+      height: 80,
+      
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: 0,
+        left: 0,
+        child: Text('Create Account', textAlign: TextAlign.left, style: TextStyle(
+        color: Color.fromRGBO(1, 15, 7, 1),
+        fontFamily: 'Yu Gothic UI',
+        fontSize: 34,
+        letterSpacing: 0.2199999988079071,
+        fontWeight: FontWeight.normal,
+        height: 1.2352941176470589
+      ),)
+      ),Positioned(
+        top: 45,
+        left: 0,
+        child: Text('Enter your Name, Email and Password for sign up.', textAlign: TextAlign.left, style: TextStyle(
+        color: Color.fromRGBO(134, 134, 134, 1),
+        fontFamily: 'Yu Gothic UI',
+        fontSize: 16,
+        letterSpacing: -0.4000000059604645,
+        fontWeight: FontWeight.normal,
+        height: 1.5
+      ),)
+      ),Positioned(
+        top: 69,
+        left: 85,
+        child: Text('Already have account?', textAlign: TextAlign.left, style: TextStyle(
+        color: Color.fromRGBO(221, 3, 38, 1),
+        fontFamily: 'Yu Gothic UI',
+        fontSize: 16,
+        letterSpacing: -0.4000000059604645,
+        fontWeight: FontWeight.normal,
+        height: 1.5
+      ),)
+      ),
+        ]
+      )
+    )
+      ),Positioned(
+        top: 623,
+        left: 35,
+        child: Container(
+      width: 335,
+      height: 140,
+      
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: 0,
+        left: 148,
+        child: Text('Or', textAlign: TextAlign.center, style: TextStyle(
+        color: Color.fromRGBO(1, 15, 7, 1),
+        fontFamily: 'Yu Gothic UI',
+        fontSize: 16,
+        letterSpacing: -0.4000000059604645,
+        fontWeight: FontWeight.normal,
+        height: 1.5
+      ),)
+      ),Positioned(
+        top: 36,
+        left: 0,
+        child: null
+      ),Positioned(
+        top: 96,
+        left: 0,
+        child: null
+      ),
+        ]
+      )
+    )
+      ),Positioned(
+        top: 226,
+        left: 35,
+        child: Container(
+      width: 335,
+      height: 363,
+      
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: 0,
+        left: 0,
+        child: null
+      ),Positioned(
+        top: 83,
+        left: 0,
+        child: null
+      ),Positioned(
+        top: 166,
+        left: 0,
+        child: null
+      ),Positioned(
+        top: 255,
+        left: 0,
+        child: null
+      ),Positioned(
+        top: 327,
+        left: 26,
+        child: Text('By Signing up you agree to our Terms Conditions & Privacy Policy.', textAlign: TextAlign.center, style: TextStyle(
+        color: Color.fromRGBO(134, 134, 134, 1),
+        fontFamily: 'Yu Gothic UI',
+        fontSize: 16,
+        letterSpacing: -0.4000000059604645,
+        fontWeight: FontWeight.normal,
+        height: 1.5
+      ),)
+      ),
+        ]
+      )
+    )
+      ),Positioned(
+        top: 27,
+        left: 31,
+        child: Container(
+        width: 127,
+        height: 51,
+        decoration: BoxDecoration(
+          image : DecorationImage(
+          image: AssetImage('assets/images/Spelzofinallogofile_pngphotoroom2.png'),
+          fit: BoxFit.fitWidth
+      ),
+  )
+      )
+      ),Positioned(
+        top: -33,
+        left: 296.213134765625,
+        child: Transform.rotate(
+        angle: -21.146704523307232 * (math.pi / 180),
+        child: Container(
+        width: 155.8198699951172,
+        height: 155.8198699951172,
+        decoration: BoxDecoration(
+          image : DecorationImage(
+          image: AssetImage('assets/images/Chatgptimagejun12202505_50_54pm1.png'),
+          fit: BoxFit.fitWidth
+      ),
+  )
+      ),
+      )
+      ),
+        ]
+      )
+    )
